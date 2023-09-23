8ce6f30a0da98419a230ca2e6e61eca0095183d8
PkTheCoda <pranavkonjeti@gmail.com>
2023-09-23 10:14:36 -0400

    finished up features section

--- src/app/Components/HeroComponent.jsx
+++ src/app/Components/HeroComponent.jsx
@@ -1,10 +1,12 @@
 "use client"
+import Image from 'next/image'
+import dashboard from '../Images/dashboardother.svg'
 
 export default function HeroComponent() {
     return (
         <>
-        <div className="bg-gradient-to-b from-[#e9eefa] from-60% border-b-2 border-red-500 to-white py-16 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-7xl mx-auto text-center flex flex-col items-center justify-center mb-40 gap-y-2">
+        <div className="bg-gradient-to-b from-[#e9eefa] from-60% to-white py-16 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
+            <div className="max-w-7xl mx-auto text-center flex flex-col items-center justify-center gap-y-2 relative">
 
                 <div>
                     <h1 className="text-4xl font-[600] font-inter tracking-tight text-gray-900 sm:text-7xl">
@@ -20,12 +22,19 @@ export default function HeroComponent() {
                 Find the perfect volunteer opportunities in <b>minutes, not days.</b> <br /> The ultimate site for finding and getting volunteer hours.
                 </p>
 
-                <button className="px-6 py-3 bg-black font-inter transition transform hover:scale-110 mt-4 text-white rounded-lg">
-                    Start Volunteering For Free.
-                </button>
-                <p className="mt-3 text-xs font-inter">No Payment required*</p>
+                <div>
+                    <button className="px-6 py-3 bg-black font-inter transition transform hover:scale-110 mt-4 text-white rounded-lg">
+                        Start Volunteering For Free.
+                    </button>
+                    <p className="mt-3 text-xs font-inter">No Payment required*</p>
+                </div>
             </div>
 
+            <Image 
+                src={dashboard}
+                className='w-auto h-auto'
+                alt="Test"
+            />
 
         </div>
         </>
--- src/app/Components/InitialFeatures.jsx
+++ src/app/Components/InitialFeatures.jsx
@@ -1,66 +1,98 @@
+"use client"
+
 import React from "react";
-import { FaCheckCircle } from "react-icons/fa"; // Replace with the actual react-icons you want to use
+import {
+    FaHandsHelping,
+    FaBuilding,
+    FaGraduationCap,
+    FaBusinessTime,
+    FaLightbulb,
+    FaClipboardCheck,
+} from "react-icons/fa";
+
+const colorClasses = {
+    blue: "hover:border-blue-400",
+    red: "hover:border-red-400",
+    yellow: "hover:border-yellow-400",
+    green: "hover:border-green-400",
+    purple: "hover:border-purple-400",
+    pink: "hover:border-pink-400",
+};
+  
+
 
 const featuresData = [
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 1",
-    subtitle: "Lorem ipsum dolor sit amet.",
+    icon: <FaHandsHelping />,
+    title: "Find Volunteer Opportunities",
+    subtitle: "Discover a wide range of volunteering options tailored to your interests and location.",
+    color: 'blue'
   },
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 2",
-    subtitle: "Consectetur adipiscing elit.",
+    icon: <FaBuilding />,
+    title: "Connect with Local Businesses",
+    subtitle: "Businesses in your area are looking for volunteers like you to support their initiatives.",
+    color: 'red'
   },
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 3",
-    subtitle: "Sed do eiusmod tempor incididunt.",
+    icon: <FaGraduationCap />,
+    title: "Streamline Your CAS Experience",
+    subtitle: "Simplify the CAS service component by easily finding and applying for volunteer opportunities.",
+    color: 'yellow'
   },
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 4",
-    subtitle: "Ut labore et dolore magna aliqua.",
+    icon: <FaBusinessTime />,
+    title: "Businesses and Schools Unite",
+    subtitle: "Bridge the gap between businesses seeking help and IB schools with motivated students ready to volunteer.",
+    color: 'green'
   },
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 5",
-    subtitle: "Duis aute irure dolor in reprehenderit.",
+    icon: <FaLightbulb />,
+    title: "Create Your Own Initiatives",
+    subtitle: "Empower students to take initiative by proposing their own volunteer projects.",
+    color: 'purple'
   },
   {
-    icon: <FaCheckCircle />,
-    title: "Feature 6",
-    subtitle: "Excepteur sint occaecat cupidatat non.",
+    icon: <FaClipboardCheck />,
+    title: "Effortless Application Process",
+    subtitle: "Apply to volunteer opportunities with ease and track your applications conveniently.",
+    color: 'pink'
   },
 ];
 
 export default function Features() {
-  return (
-    <div className="bg-[#e9eefa] py-16">
-      <div className="max-w-7xl mx-auto">
-        <h2 className="text-5xl font-medium mb-2 font-inter">Find the <span className="bg-yellow-400 rounded-xl p-1">Perfect</span> Opportunities</h2>
-        <p className="text-lg text-gray-600 mb-12 font-inter">
-          Stella offers a variety of features to turbo-charge the volunteering process.
-        </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {featuresData.map((feature, index) => (
-            <div
-              key={index}
-              className="bg-white p-6 rounded-lg shadow-md shadow-blue-200/50 font-inter"
-            >
-              <div className="text-3xl mb-4 text-blue-500">
-                {feature.icon}
-              </div>
-              <h3 className="text-xl font-semibold mb-2">
-                {feature.title}
-              </h3>
-              <p className="text-gray-600">
-                {feature.subtitle}
+    return (
+      <div className="bg-[#e9eefa] py-16">
+        <div className="max-w-7xl mx-auto">
+          <div className="text-center">
+              <h2 className="text-5xl font-semibold mb-2 tracking-tight font-inter">Find <span className="underline">your</span> best fit.</h2>
+              <p className="text-md text-gray-600 mb-12 font-inter">
+              Stella offers a variety of features to <br /> turbo-charge the volunteering process.
               </p>
-            </div>
-          ))}
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mx-20">
+            {featuresData.map((feature, index) => (
+              <div
+                key={index}
+                className={`bg-white px-6 py-6 rounded-xl border-2 border-transparent transition duration-500 ${colorClasses[feature.color]}`}
+              >
+                <div className="text-3xl mb-4 text-blue-500 bg-blue-100 p-4 w-max rounded-full">
+                  {feature.icon}
+                </div>
+                <h3 className="text-xl mb-2 font-outfit">
+                  {feature.title}
+                </h3>
+                <p className="text-gray-600 font-inter">
+                  {feature.subtitle}
+                </p>
+
+                <p className="hover:underline text-black font-outfit cursor-pointer mt-4">
+                    Try Now
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
