74be37d6a61a0698cfe8774d828a6941072a9b80
PkTheCoda <pranavkonjeti@gmail.com>
2023-09-22 20:10:25 -0400

    preloaded + added fonts to website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,16 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Outfit } from "next/font/google"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const outfit = Outfit({
+  subsets: ['latin'],
+  variable: '--font-outfit',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +19,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${outfit.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import Image from 'next/image'
+import './globals.css'
 
-export default function Home() {
-  return (
-    <>
-      <div className='bg-red-500'>
-        Hi!
-      </div>
-    </>
-  )
-}
+
+export default function Page() {
+    return (
+        <>
+            <main>
+                <div className='font-inter text-7xl font-semibold'>
+                    This is a test
+                </div>
+            </main>
+        </>
+    )
+} 
