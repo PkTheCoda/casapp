8d49e421f5591a75e31da761745ea572d6f18d39
PkTheCoda <pranavkonjeti@gmail.com>
2023-09-23 18:35:45 -0400

    added blur line

--- src/app/Components/HeroComponent.jsx
+++ src/app/Components/HeroComponent.jsx
@@ -6,7 +6,7 @@ export default function HeroComponent() {
     return (
         <>
             <div className='pb-20 relative'>
-                <div className="bg-[#e9eefa] pt-16 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
+                <div className="bg-[#e9eefa] pt-16 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center z-40">
                     <div className="max-w-7xl mx-auto text-center flex flex-col items-center justify-center gap-y-2 relative">
 
                         <div>
@@ -24,7 +24,7 @@ export default function HeroComponent() {
                         </p>
 
                         <div>
-                            <button className="px-6 py-3 z- bg-black font-inter transition transform hover:scale-110 mt-4 text-white rounded-lg">
+                            <button className="z-[999] px-6 py-3 z- bg-black font-inter transition transform hover:scale-110 mt-4 text-white rounded-lg">
                                 Start Volunteering
                             </button>
                             <p className="mt-3 text-xs font-inter">No Payment required*</p>
@@ -42,31 +42,33 @@ export default function HeroComponent() {
                 </div>
             </div>
 
-        <div
-            className="absolute inset-x-0 -top-40 z-10 transform-gpu overflow-hidden blur-2xl sm:-top-[5rem]"
-            aria-hidden="true"
-            >
             <div
-                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                style={{
-                clipPath:
-                    'polygon(28.22% 33.13%, 37.2% 26.09%, 48.37% 23.76%, 60.27% 26.09%, 66.68% 29.23%, 70.05% 35.52%, 72.34% 46.86%, 71.78% 53.33%, 70.05% 61.09%, 62.84% 64.45%, 51.31% 69.45%, 38.91% 67.34%, 28.22% 57.35%, 23.5% 43.51%)',
-                }}
-            />
-        </div>
+                className="absolute inset-x-0 -top-40 z-10 transform-gpu overflow-hidden blur-2xl sm:top-[5rem] w-[30rem] h-max"
+                aria-hidden="true"
+                >
+                <div
+                    className="pointer-events-none relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500 opacity-30 sm:left-[1rem] sm:w-[72.1875rem]"
+                    style={{
+                    clipPath:
+                        'polygon(42.64% 13.22%, 67.05% 8.43%, 88.27% 13.22%, 95.85% 29.9%, 88.27% 54.16%, 85.86% 74.44%, 77.88% 87.47%, 58.91% 88.7%, 44.01% 91.62%, 24.38% 99.67%, 13.46% 86.95%, 33.65% 48.25%, 33.65% 36.41%)',
+                        pointerEvents: 'none'
+                    }}
+                    
+                />
+            </div>
 
-        <div
+        {/* <div
             className="absolute inset-x-0 -top-40 z-10 transform-gpu overflow-hidden blur-3xl sm:top-[15rem]"
             aria-hidden="true"
             >
             <div
-                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-red-700 via-red-300 to-blue-500 opacity-30 sm:left-[90rem] sm:w-[72.1875rem]"
+                className="pointer-events-none relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-400 via-red-300 to-orange-500 opacity-30 sm:left-[90rem] sm:w-[72.1875rem]"
                 style={{
                 clipPath:
-                    'polygon(29.43% 29.68%, 39.76% 20.45%, 53.62% 20.45%, 62.15% 25.67%, 70.77% 29.68%, 78.79% 33.4%, 82.55% 40.88%, 84.16% 54.27%, 85.78% 67.69%, 79.3% 79.55%, 70.03% 84.96%, 59.38% 83.54%, 44.14% 83.54%, 29.43% 79.55%, 23.94% 72.57%, 16.26% 54.15%, 20.92% 40.88%)',
+                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                 }}
             />
-        </div>
+        </div> */}
 
 
 
