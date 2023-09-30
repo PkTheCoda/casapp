cbacde7ea4ee347ff0cf9e5b3930a4a539aadc4d
PkTheCoda <pranavkonjeti@gmail.com>
2023-09-29 23:02:45 -0400

    removed ' character causing error and changed cta

--- src/app/business/BusinessCta.jsx
+++ src/app/business/BusinessCta.jsx
@@ -4,10 +4,10 @@ export default function BusinessCta() {
     return (
         <>
             <div className="my-20 flex flex-col gap-y-10 items-center justify-center">
-                <div className="font-sans font-semibold text-6xl text-center">It's your business' time to Shine 🌟</div>
+                <div className="font-sans font-semibold text-6xl text-center">It&apos;s your business&apos; time to <span className="shine-gradient">Shine</span> 🌟</div>
 
                 <button
-                    className="bg-slate-200 p-4 font-sans rounded-lg w-max text-slate-600 transition transform hover:scale-110 font-semibold"
+                    className="bg-gradient-to-br from-blue-500 via-blue-600 to-gray-900 shadow-md p-4 font-sans rounded-lg w-max text-white transition transform hover:scale-110 font-semibold"
                 >
                     Make a Business Account
                 </button>
