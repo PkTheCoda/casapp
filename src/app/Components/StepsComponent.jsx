40cbc728c2e3e53bbbb00a0c37ac157d0a20b9fe
PkTheCoda <pranavkonjeti@gmail.com>
2023-10-10 22:25:34 -0400

    decreased margin

--- src/app/Components/StepsComponent.jsx
+++ src/app/Components/StepsComponent.jsx
@@ -8,7 +8,7 @@ export default function StepComponent() {
           <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <div>
-            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold font-sans tracking-wider text-slate-600 uppercase rounded-full bg-teal-accent-400">
+            <p className="inline-block px-3 py-px mb-2 text-xs font-semibold font-sans tracking-wider text-slate-600 uppercase rounded-full bg-teal-accent-400">
               Volunteering Made <span className="font-semibold text-blue-500">Simple.</span>
             </p>
           </div>
