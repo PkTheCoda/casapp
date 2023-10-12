b09be9392d27df69c1ab0b85a721a404a061dc61
PkTheCoda <pranavkonjeti@gmail.com>
2023-10-12 18:50:49 -0400

    added our solution part

--- src/app/about/AboutSection1.jsx
+++ src/app/about/AboutSection1.jsx
@@ -23,7 +23,7 @@ export default function AboutSection2() {
 
     return (
         <>
-            <div className="h-max flex flex-col items-center justify-center py-20">
+            <div className="h-max flex flex-col items-center justify-center py-40">
                 <div className="text-4xl lg:text-5xl font-manrope font-extrabold text-center">
                     What&apos;s <span className="text-gradient-full">The Problem?</span>
                 </div>
@@ -41,6 +41,146 @@ export default function AboutSection2() {
                             <p className='text-lg text-blue-100 cursor-pointer   underline font-sans'>View Source</p>
                         </div>
                     ))}
+                </div>
+
+                <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                    <div className="grid gap-5 row-gap-10 lg:grid-cols-2">
+                        <div className="flex flex-col justify-center">
+                        <div className="max-w-xl mb-6">
+                            <h2 className="max-w-lg mb-6 font-manrope text-3xl font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-none">
+                            Our <span className='text-gradient-full'>Solution</span>
+                            </h2>
+                            <p className="text-base text-gray-700 md:text-lg">
+                            Stella offers a solution for just this - we connect students and businesses together
+                            and make the process easier. 
+                            </p>
+                        </div>
+                        <p className="mb-4 text-sm font-extrabold font-manrope tracking-widest uppercase">
+                            Features
+                        </p>
+                        <div className="grid space-y-3 sm:gap-2 sm:grid-cols-2 sm:space-y-0">
+                            <ul className="space-y-3">
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Easy to Use
+                            </li>
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Intuitive System
+                            </li>
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Free to Use
+                            </li>
+                            </ul>
+                            <ul className="space-y-3">
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Easily Filterable Database
+                            </li>
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Built for CAS & IB
+                            </li>
+                            <li className="flex font-inter">
+                                <span className="mr-1">
+                                <svg
+                                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                                    stroke="currentColor"
+                                    viewBox="0 0 52 52"
+                                >
+                                    <polygon
+                                    strokeWidth="4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                    />
+                                </svg>
+                                </span>
+                                Manage all in one place
+                            </li>
+                            </ul>
+                        </div>
+                        </div>
+                        <div>
+                        <img
+                            className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+                            src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                            alt=""
+                        />
+                        </div>
+                    </div>
                 </div>
                 
             </div>
